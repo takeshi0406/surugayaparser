acf2d14bc74a9fd63e028b7966c4d322989eeca6
takeshi <sci.and.eng@gmail.com>
2016-06-19 17:41:02 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='surugayaparser',
     version='0.1.0',
+    author='takeshi0406',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'BeautifulSoup'
+        'beautifulsoup4'
     ]
 )
